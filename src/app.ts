02225a5d5e201b3f82406ecb4a6e329b27379e64
Matheeo <90444801+Matheeo@users.noreply.github.com>
2025-01-22 21:30:29 +0100

    [DEV] project setup w/ fastify & prisma & postegre container

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: { level: 'warn' } });
+
+fastify.get('/stats', async (request, reply) => {
+    return { message: 'Welcome to my statistique API!' };
+});
+
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 });
+        console.log('Server running on http://localhost:3000/stats');
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
