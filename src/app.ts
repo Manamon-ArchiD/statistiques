29f6a08584290a1f960da8601276df3f96662974
Matheeo <90444801+Matheeo@users.noreply.github.com>
2025-01-23 16:55:55 +0100

    [DEV] dockerfile

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,8 @@ fastify.register(summarizeRoutes);
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 });
-        console.log('Server running on http://localhost:3000/stats');
+        await fastify.listen({ port: 3000 , host: '0.0.0.0'});
+        console.log('Server running!');
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
